fdce5a69bdabb4c472f00d1747afddcdb3384da6
tony <aacombarro89@gmail.com>
2020-09-18 18:48:37 -0400

    - added a way to difference between several mention directives on the same component by using exportAs - added an example for testing a button trigger flow for angular-mentions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,17 +27,37 @@ export class AppComponent {
   }
 
   insertAtCaret(id, text) {
-    const element = document.getElementById(id);
-    element.focus();
+    const myField: any = document.getElementById(id);
     setTimeout(() => {
-      const selection = window.getSelection();
-      const range = selection.getRangeAt(0);
-      range.deleteContents();
-      const node = document.createTextNode(text);
-      range.insertNode(node);
+      const _document: any = document;
 
-      selection.collapseToEnd();
+      if (_document.selection) {
+        myField.focus();
+        const sel = _document.selection.createRange();
+        sel.text = text;
+      } else {
+        const selection = window.getSelection();
+        const range = selection.getRangeAt(0);
+        const startPos = range.startOffset;
+        const endPos = range.endOffset;
+        myField.innerText = myField.innerText.substring(0, startPos)
+          + text
+          + myField.innerText.substring(endPos, myField.innerText.length);
+
+        this.setCaret(myField, startPos + text.length);
+      }
     }, 10);
+  }
+
+  setCaret(el, offset) {
+    const range = document.createRange();
+    const sel = window.getSelection();
+
+    range.setStart(el.childNodes[0], offset);
+    range.collapse(true);
+
+    sel.removeAllRanges();
+    sel.addRange(range);
   }
 
   onOpenTagsDropdown($event) {
